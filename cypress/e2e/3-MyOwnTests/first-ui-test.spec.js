f57745f93976a8324cadf1475ecf62215de4f231
Tyxa <tyxa2017@gmail.com>
2023-11-26 17:06:08 +0200

    Update cypress/e2e/3-MyOwnTests/first-ui-test.spec.js
    Co-authored-by: Artem <shoiko.a.s@gmail.com>

--- cypress/e2e/3-MyOwnTests/first-ui-test.spec.js
+++ cypress/e2e/3-MyOwnTests/first-ui-test.spec.js
@@ -30,7 +30,7 @@ beforeEach(() =>{
     cy.visit('/layout/accordion')})
 
 it ('Toggle first item test', ()=> {
-    cy.get('nb-accordion-item-body').first().should('not.be.visible');
+    cy.get('nb-accordion-item-body').first().should('be.hidden');
     cy.get('button.appearance-filled').click()
     cy.get('nb-accordion-item-body').first().should('be.visible')
  })
