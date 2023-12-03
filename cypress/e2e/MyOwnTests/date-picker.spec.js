9a21fe3c85f606d21d4af47fc6de7c694a3aaa5c
Artour <tyxa2017@gmail.com>
2023-12-03 11:25:27 +0200

    const position update

--- cypress/e2e/MyOwnTests/date-picker.spec.js
+++ cypress/e2e/MyOwnTests/date-picker.spec.js
@@ -3,12 +3,11 @@ import {DatePickerPage} from '../../../pages/Date-picker.page'
 import moment from "moment"
 describe('Date picker tests with PageObject', ()=> {
 const datePickerPage = new DatePickerPage()
-
+const today = moment().format('ll')
 it('Date picker today day check and compare value between input and present time', ()=>{
     datePickerPage.DatePickerPageOpen ()
     datePickerPage.formPicker.click()
     datePickerPage.todayDayButton.click()
-    const today = moment().format('ll')
     datePickerPage.formPicker.should('have.value', today)
 }) 
 })
