c3d8b2b0ec7055a6a54929a2f70c39f27f66f008
Artour <tyxa2017@gmail.com>
2023-12-05 13:22:27 +0200

    lesson 16 homework

---
+++ cypress/e2e/MyOwnTests/cy-request.spec.js
@@ -0,0 +1,36 @@
+/// <reference types = "cypress" />
+
+describe('Tests for GET, POST, PUT, PATCH , DELETE and status code checking ', () =>{
+      
+it('We use method POST for creating post on the website', ()=> {
+cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', { 
+title: 'Artour', 
+body: 'Post',
+userId: 15
+}).then (res => {expect (res.status).to.eq(201)})
+})
+
+it('We use method GET for getting posts from the website', ()=> {
+cy.request('GET', 'https://jsonplaceholder.typicode.com/posts',)
+.then (res => {expect (res.status).to.eq(200)})
+})
+
+it('We use method PUT for updating post on the website', ()=> {
+cy.request('PUT', 'https://jsonplaceholder.typicode.com/posts/99',{    
+title: 'Artour',
+body: 'PUT',
+userId: 4   
+}).then (res => {expect (res.status).to.eq(200)})
+})
+
+it('We use method PATCH for updating post on the website', ()=> {
+cy.request('PATCH', 'https://jsonplaceholder.typicode.com/posts/99',{    
+title: 'Patch Name',  
+}).then (res => {expect (res.status).to.eq(200)})
+})
+
+it('We use method DELETE for delete post on the website', ()=> {
+cy.request('DELETE', 'https://jsonplaceholder.typicode.com/posts/99')
+.then (res => {expect (res.status).to.eq(200)})
+})
+})
